eaae73f0702f28aff2e0e81cfdad3b10a1fd9e1b
NathanJ4620 <nathanjseeley@gmail.com>
2019-09-06 21:23:07 -0600

    this commit has changes that I forget so...

---
+++ RPC.js
@@ -0,0 +1,11 @@
+function computerPlay (){
+    let index = Math.floor(Math.random() * 3);
+    let rpc = ["Rock", "Paper", "Scissors"];
+    return rpc[index];
+}
+
+console.log(computerPlay());
+
+function playRound (playerSelection, computerSelection){
+    
+}
